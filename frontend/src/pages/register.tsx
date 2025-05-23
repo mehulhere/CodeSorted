4fb14a379a64ebe1772ac533f2bb269988a5c837
mehulhere <themehulpahuja@gmail.com>
2025-05-23 08:16:06 +0530

    feat(fronend): add user registration page

---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function Register() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault(); // Prevents default form submission
+
+        if (password !== confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+
+        if (password.length < 8) {
+            alert('Password must be at least 8 characters long');
+            return;
+        }
+
+        // try {
+        //     const response = await fetch
+        // }
+    }
+}
